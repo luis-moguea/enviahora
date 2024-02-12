3b0b0cfdecb0cd2ac76867eba0d42b1bdb4a0502
luis-moguea <luis.mogueadp@gmail.com>
2024-02-12 17:47:00 -0500

    update map

--- app/components/MapLocal.tsx
+++ app/components/MapLocal.tsx
@@ -21,7 +21,7 @@ const MapLocal = () => {
       alignItems="center"
     >
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d3966.097334510032!2d-75.57128297623592!3d6.250904193737539!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x8e4428f8d2bbf907%3A0x3c15d73310c5f9b4!2sParque%20Berr%C3%ADo%2C%20La%20Candelaria%2C%20Medell%C3%ADn%2C%20Antioquia!3m2!1d6.2504520999999995!2d-75.56823589999999!4m5!1s0x8e4428fed74c3a65%3A0xf334493e0fb062fa!2sCentro%20Comercial%20Veracruz%2C%20Av.%20Carabobo%20%2351-52%2C%20La%20Candelaria%2C%20Medell%C3%ADn%2C%20La%20Candelaria%2C%20Medell%C3%ADn%2C%20Antioquia!3m2!1d6.2513394!2d-75.5690375!5e0!3m2!1ses-419!2sco!4v1706385948467!5m2!1ses-419!2sco"
+        src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d3966.0973345102298!2d-75.57128833981982!3d6.250904193711421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x8e4428f8d2bbf907%3A0x3c15d73310c5f9b4!2sParque%20Berr%C3%ADo%2C%20La%20Candelaria%2C%20Medell%C3%ADn%2C%20Antioquia!3m2!1d6.2504520999999995!2d-75.56823589999999!4m5!1s0x8e4429eedcb34fdb%3A0x7d9981c80624e051!2sEnviahora%2C%20Centro%20comercial%20Veracruz%2C%20Av.%20Carabobo%20%2351-52%20local%203233%2C%20La%20Candelaria%2C%20Medell%C3%ADn%2C%20La%20Candelaria%2C%20Medell%C3%ADn%2C%20Antioquia!3m2!1d6.251331899999999!2d-75.56901239999999!5e0!3m2!1ses-419!2sco!4v1707777769581!5m2!1ses-419!2sco"
         width="90%"
         height="450"
         style={iframeStyle}
